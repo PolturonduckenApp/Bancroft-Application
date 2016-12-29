ed3f63184c2305428d20b5503365c88c129cf2d3
Andrew Turley <aturley123@gmail.com>
2016-12-29 12:27:25 -0700

    Better organized import statements

--- src/com/polturonducken/bancroft/Bancroft.java
+++ src/com/polturonducken/bancroft/Bancroft.java
@@ -3,21 +3,21 @@ package com.polturonducken.bancroft;
 import com.codename1.ui.Display;
 import com.codename1.ui.Form;
 import com.codename1.ui.TextArea;
-import com.codename1.ui.plaf.UIManager;
-import com.codename1.ui.util.Resources;
 import com.codename1.ui.Command;
 import com.codename1.ui.FontImage;
 import com.codename1.ui.Image;
 import com.codename1.ui.Label;
-import com.codename1.ui.*;
 import com.codename1.ui.NavigationCommand;
 import com.codename1.ui.TextArea;
 import com.codename1.ui.TextField;
 import com.codename1.ui.Toolbar;
+import com.codename1.ui.Button;
+import com.codename1.ui.plaf.UIManager;
+import com.codename1.ui.util.Resources;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.layouts.BoxLayout;
-import com.codename1.components.WebBrowser;
 import com.codename1.ui.layouts.*;
+import com.codename1.components.WebBrowser;
 import com.codename1.notifications.LocalNotification;
 
 /**
