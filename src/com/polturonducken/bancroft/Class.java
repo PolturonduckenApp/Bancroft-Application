69b5c896ff3699623d04b463adc43f112fb58ea5
aturley <aturley@CounterfeitLlama-MintD>
2017-01-15 19:08:02 -0500

    Changed letter day to an array

--- src/com/polturonducken/bancroft/Class.java
+++ src/com/polturonducken/bancroft/Class.java
@@ -4,11 +4,11 @@ import java.util.Date;
 
 public class Class extends Block {
 	private int period;
-	private String letterDay;
+	private String[] letterDay;
 	private CalEvent calEvent;
 	private boolean special;
 	
-	public Class(String name, Date endTime, Date startTime, int period, String letterDay, CalEvent calEvent) {
+	public Class(String name, Date endTime, Date startTime, int period, String[] letterDay, CalEvent calEvent) {
 		super(name, endTime, startTime);
 		this.period = period;
 		this.letterDay = letterDay;
@@ -23,8 +23,8 @@ public class Class extends Block {
 	public int getPeriod() {return period;}
 	public void setPeriod(int period) {this.period = period;}
 	
-	public String getLetterDay() {return letterDay;}
-	public void setLetterDay(String letterDay) {this.letterDay = letterDay;}
+	public String[] getLetterDay() {return letterDay;}
+	public void setLetterDay(String[] letterDay) {this.letterDay = letterDay;}
 	
 	public CalEvent getCalEvent() {return calEvent;}
 }
